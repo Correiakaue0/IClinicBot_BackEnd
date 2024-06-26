de14533e44a9820eff9d848ababd88d3a792cd81
Kauê <kaue.correia@harmonit.com.br>
2024-06-26 11:45:37 -0300

    enums

---
+++ IClinicBot/Domain/Enums/EnumExamType.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums
+{
+    public enum EnumExamType
+    {
+        Hemograma,
+        RaioX,
+        Eletrocardiograma,
+        Audiometria
+    }
+}
---
+++ IClinicBot/Domain/Enums/EnumStatusMedicalAppointment.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Enums
+{
+    public enum EnumStatusMedicalAppointment
+    {
+        Completed,
+        Rescheduled,
+        Missed
+    }
+}
